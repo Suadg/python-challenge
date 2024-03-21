0eaf7dd84d509b39b1b6b692107e2ada2f308781
Suad G <162393107+Suadg@users.noreply.github.com>
2024-03-21 17:57:13 -0400

    Add line code # 2-14

--- PayBank/main.py
+++ PayBank/main.py
@@ -1,2 +1,15 @@
+# Import modules
+import os
 import csv
-budget_
+import pandas as pd
+
+# Capture path in variable
+budget_csv = os.path.join('..', 'PyBank', 'Resources', 'budget_data.csv')
+
+# Use pandas to read data
+df = pd.read_csv('budget_data.csv')
+
+# Count number of months
+number_of_months = len(df)
+print("Total months: ", number_of_months)
+
