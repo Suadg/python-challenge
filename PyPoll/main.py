ac68cc3208fa3466e0f99088dd4c67f7eb012fe4
Suadg <162393107+Suadg@users.noreply.github.com>
2024-07-30 00:56:28 -0400

    update pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ import os
 import csv
 
 # Specify the path to the CSV file election_data.csv
-electionData_csv = os.path.join('Resources', 'election_data.csv')
+election_csv = os.path.join('Resources', 'election_data.csv')
 
 # Variables to store total votes
 totalVotes = 0
@@ -12,39 +12,26 @@ totalVotes = 0
 candidates_votes = {}
 
 # Open and read the data of the CSV file.
-with open(electionData_csv) as data:
-
-    # Initiate the reader
+with open(election_csv) as data:
+# Initiate the reader
     reader = csv.reader(data, delimiter=",")
-
-    # Skip the header row when looping through the data
+# Skip the header row when looping through the data
     header = next(reader)
+# Start your loop over the rows to make calculations
+for row in reader:
+# Get the candidate name from the current row.
+# Names are in index 2
+    candidate_name = str(row[2])
 
-    #--------------------------------------------------
-    #-----------         L  O  O  P         -----------
-    #--------------------------------------------------
+# Check if candidate name is in the dictionary. If not, adds candidates with initial count of zero
+if candidate_name not in candidates_votes:
+    candidates_votes[candidate_name] = 0
 
-    # Start your loop over the rows to make calculations
-    for row in reader:
+#Add votes to each candidate in increments of 1 using += operator
+candidates_votes[candidate_name] += 1
 
-        # Get the candidate name from the current row.
-        # Names are in index 2
-        candidate_name = str(row[2])
-
-        # -----------------------------------------------
-        # ----------        Conditionals        ---------
-        # -----------------------------------------------
-
-        # Check if candidate name is in the dictionary
-        #If not, adds candidates with initial count of cero
-        if candidate_name not in candidates_votes:
-            candidates_votes[candidate_name] = 0
-
-        #Add votes to each candidate in increments of 1 using += operator
-        candidates_votes[candidate_name] += 1
-
-        #Calculate the total votes in increments of 1 using += operator
-        totalVotes += 1
+#Calculate the total votes in increments of 1 using += operator
+totalVotes += 1
 
 
 # Print the outcomes in the console
@@ -58,14 +45,15 @@ print("-------------------------")
 #otherwise, it won't print all of the candidates in the format required
 for candidate_name, totalVotesCandidate in candidates_votes.items():
 
-    #Calculate the percentage of each candidate's votes
+#Calculate the percentage of each candidate's votes
     percent = (totalVotesCandidate / totalVotes) * 100
-    #Format the percentage to round to 3 decimals
-    percentFormat = float(round(percent,3))
 
-    #Variable to save the results to print out
-    result = f"{candidate_name}: {percentFormat}% ({totalVotesCandidate})"
-    print(result)
+#Format the percentage to round to 3 decimals
+percentFormat = float(round(percent,3))
+
+#Variable to save the results to print out
+result = f"{candidate_name}: {percentFormat}% ({totalVotesCandidate})"
+print(result)
 
 #Finding out who won and printing it out to console
 #Store in 'winner' variable. Use the max function for the maximum value
@@ -77,7 +65,7 @@ print(f"Winner: {winner}")
 print("-------------------------")
 
 #--------------------------------------------------------------------------
-#------       Printing the resulting analysis into a text file       ------
+#------       Print the resulting analysis into a text file       ------
 #--------------------------------------------------------------------------
 
 #Specify the output file path
@@ -86,9 +74,9 @@ output_path = os.path.join("analysis", "PyPoll_analysis.txt")
 # Open the file in write mode and redirect the print statements to the file
 with open(output_path, "w") as textfile:
 
-    #Inicialize the txt.writer function to write the variable we created
+#Inicialize the txt.writer function to write the variable we created
     csvwriter = csv.writer(textfile)
 
-    #Writing the 'Election Results' and the 'Total Votes' to the txt file
-    #csvwriter.writerow([""])  Write within parenthesis the outcomes to be written
+#Writing the 'Election Results' and the 'Total Votes' to the txt file
+#csvwriter.writerow([""])  Write within parenthesis the outcomes to be written
     csvwriter.writerow(["Election Results"])
